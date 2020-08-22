483d720f20037e3e0eaa3643c0a9055c60306a22
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-08-21 21:33:15 -0400

    add dummy versions of all endpoints

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github-terraform-registry-proxy
 
 go 1.14
+
+require github.com/gorilla/mux v1.7.4
--- main.go
+++ main.go
@@ -2,14 +2,53 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
+	r := mux.NewRouter()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "ok")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r.HandleFunc("/.well-known/terraform.json", TerraformWellKnownHandler)
+	r.HandleFunc("/{namespace}/{name}/{provider}/versions", ListModuleVersionsHandler)
+	r.HandleFunc("/{namespace}/{name}/{provider}/{system}/{version}/download", DownloadModuleHandler)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func TerraformWellKnownHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `{"modules.v1":"/"}`)
+}
+
+func ListModuleVersionsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	namespace := vars["namespace"]
+	name := vars["name"]
+	provider := vars["provider"]
+
+	fmt.Println("namespace", namespace)
+	fmt.Println("name", name)
+	fmt.Println("provider", provider)
+
+	fmt.Fprint(w, "[]")
+}
+
+func DownloadModuleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	namespace := vars["namespace"]
+	name := vars["name"]
+	provider := vars["provider"]
+	system := vars["system"]
+	version := vars["version"]
+
+	fmt.Println("namespace", namespace)
+	fmt.Println("name", name)
+	fmt.Println("provider", provider)
+	fmt.Println("system", system)
+	fmt.Println("version", version)
+
+	w.WriteHeader(204)
+	w.Header().Add("X-Terraform-Get", "/")
 }
